7b2c34733252c9920557baf4aeee2c2200754b97
Marco <ma.rodriguezf21@gmail.com>
2020-08-07 20:37:37 -0400

    Utilizando Hooks con estado en videos y en categorias

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable arrow-parens */
 import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
-import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Categories from '../components/Categories';
 import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
@@ -13,35 +14,21 @@ import '../assets/styles/App.scss';
 const API = 'http://localhost:3000/initialState';
 
 const App = () => {
-  const initialState = useInitialState(API);
+  const [videos, categories] = useInitialState(API);
   return (
     <div className='App'>
       <Header />
       <Search />
-      {
-        (initialState.mylist.length > 0) && (
-          <Categories title='Mi lista'>
-            <Carousel>
-              <CarouselItem />
-            </Carousel>
-          </Categories>
-        )
-      }
-
-      <Categories title='Tendencias'>
-        <Carousel>
-          {initialState.trends
-            .map(item => <CarouselItem key={item.id} {...item} />)}
-        </Carousel>
-      </Categories>
-
-      <Categories title='Originales de Platflix'>
-        <Carousel>
-          {initialState.originals
-            .map(item => <CarouselItem key={item.id} {...item} />)}
-        </Carousel>
-      </Categories>
-
+      {categories
+        .map(category =>
+          (videos[category].length > 0) && (
+            <Categories title={category}>
+              <Carousel>
+                {videos[category].map(video =>
+                  <CarouselItem key={video.id} {...video} />)}
+              </Carousel>
+            </Categories>
+          ))}
       <Footer />
     </div>
   );
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -8,12 +8,18 @@ const useInitialState = (API) => {
     originals: [],
   });
 
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     fetch(API)
       .then(response => response.json())
-      .then(data => setVideos(data));
+      .then(data => {
+        setVideos(data);
+        setCategories(Object.keys(data));
+      });
+
   }, []);
-  return videos;
+  return [videos, categories];
 };
 
 export default useInitialState;
